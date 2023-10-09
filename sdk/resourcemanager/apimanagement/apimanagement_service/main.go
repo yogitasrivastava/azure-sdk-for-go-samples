cb41bf9c48e3d4f7c7d280bef1b7a24cbb14c406
Yogi <115236926+yogitasrivastava@users.noreply.github.com>
2023-10-09 14:30:28 +1100

    Update main.go

--- sdk/resourcemanager/apimanagement/apimanagement_service/main.go
+++ sdk/resourcemanager/apimanagement/apimanagement_service/main.go
@@ -12,6 +12,8 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/apimanagement/armapimanagement"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/log"
+	"github.com/Azure/azure-sdk-for-go/sdk/internal/log"
 )
 
 var (
